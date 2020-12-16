6810f9eeb6591f15e9a6ac2a434ac0cdb2893eb3
joshpled <joshuapleduc@gmail.com>
2020-12-15 18:54:26 -0600

    change url for backend

--- src/actions/petActions.js
+++ src/actions/petActions.js
@@ -1,10 +1,10 @@
 import {newAmount, nameCapitalized} from '../helpers/helper-methods'
 
 // heroku deployment
-// const BASE_URL = 'https://vast-plains-99264.herokuapp.com/api/v1/'
+const BASE_URL = 'https://vast-plains-99264.herokuapp.com/api/v1/'
 
 // localhost
-const BASE_URL = "http://localhost:3090/api/v1/";
+// const BASE_URL = "http://localhost:3090/api/v1/";
 
 
 export const createNewPet = (name) => {
