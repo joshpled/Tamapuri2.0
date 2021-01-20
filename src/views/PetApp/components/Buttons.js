d072efd3785a1e2c83cb4b76ed9f6b3e7dc9b342
joshpled <joshuapleduc@gmail.com>
2021-01-20 17:56:08 -0500

    reset buttons component

--- src/views/PetApp/components/Buttons.js
+++ src/views/PetApp/components/Buttons.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Buttons extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		debugger;
+	}
+
 	render() {
 		return (
 			<div>
-			
+				<button onClick={this.handleClick}>Items</button>
 			</div>
 		);
 	}
