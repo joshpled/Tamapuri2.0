d5c0d664e481b178f8573afb2918ee1f7eb23200
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-07-21 00:54:09 -0400

    remove eslint error

--- src/state/actions/authActions.js
+++ src/state/actions/authActions.js
@@ -1,76 +1,76 @@
-import { config } from './Constants';
+import { config } from "./Constants";
 var url = config.url.AUTH_URL;
 
 export const storeUser = (payload) => {
-	return (dispatch) => {
-		dispatch({ type: 'SET_USER', payload });
-	};
+  return (dispatch) => {
+    dispatch({ type: "SET_USER", payload });
+  };
 };
 
 export const clearUser = () => {
-	return (dispatch) => {
-		fetch(url + 'logout/', {
-			method: 'DELETE',
-			headers: { 'Content-Type': 'application/json' },
-			credentials: 'include',
-		})
-			.then(() => {
-				dispatch({ type: 'CLEAR_USER' });
-			})
-			.catch((error) => console.log(error));
-	};
+  return (dispatch) => {
+    fetch(url + "logout/", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+      .then(() => {
+        dispatch({ type: "CLEAR_USER" });
+      })
+      .catch((error) => console.log(error));
+  };
 };
 
 export const loginUser = (email, password) => {
-	let loginData = {
-		email: email,
-		password: password,
-	};
+  let loginData = {
+    email: email,
+    password: password,
+  };
 
-	return (dispatch) => {
-		dispatch({ type: 'LOADING_USER' });
-		fetch(url + 'sessions/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			credentials: 'include',
-			body: JSON.stringify({ user: loginData }),
-		})
-			.then((response) => response.json())
-			.then((json) => {
-				if (json.logged_in === true) {
-					dispatch({ type: 'SET_USER', payload: json });
-					return { logged_in: true };
-				}
-			})
-			.catch((error) => console.log(error));
-	};
+  return (dispatch) => {
+    dispatch({ type: "LOADING_USER" });
+    fetch(url + "sessions/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ user: loginData }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.logged_in === true) {
+          dispatch({ type: "SET_USER", payload: json });
+          return { logged_in: true };
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 };
 
 export const registerUser = (email, password, password_confirmation) => {
-	const remove = (s) => s.replace(/\@.*$/, '');
+  const remove = (s) => s.replace(/@.*$/, "");
 
-	let registerData = {
-		email: email,
-		password: password,
-		password_confirmation: password_confirmation,
-		username: remove(email),
-	};
+  let registerData = {
+    email: email,
+    password: password,
+    password_confirmation: password_confirmation,
+    username: remove(email),
+  };
 
-	return (dispatch) => {
-		dispatch({ type: 'LOADING_USER' });
-		fetch(url + 'registrations/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			credentials: 'include',
-			body: JSON.stringify({ user: registerData }),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				if (data.status === 'created') {
-					dispatch({ type: 'SET_USER', payload: data });
-				} else {
-					dispatch({ type: 'READ_ERROR', payload: data.exception });
-				}
-			});
-	};
+  return (dispatch) => {
+    dispatch({ type: "LOADING_USER" });
+    fetch(url + "registrations/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ user: registerData }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "created") {
+          dispatch({ type: "SET_USER", payload: data });
+        } else {
+          dispatch({ type: "READ_ERROR", payload: data.exception });
+        }
+      });
+  };
 };
--- src/views/PetApp/calculations.js
+++ src/views/PetApp/calculations.js
@@ -1,61 +1,62 @@
 export function limitNumberWithinRange(num, min, max) {
-	const MIN = min || 0;
-	const MAX = max || 100;
-	const parsed = parseInt(num);
-	return Math.min(Math.max(parsed, MIN), MAX);
+  const MIN = min || 0;
+  const MAX = max || 100;
+  const parsed = parseInt(num);
+  return Math.min(Math.max(parsed, MIN), MAX);
 }
 
 let arr = Array(6000).fill(3).concat(Array(3000).fill(2), Array(100).fill(1), Array(5000).fill(0));
 
 function shuffle(a) {
-	var j, x, i;
-	for (i = a.length - 1; i > 0; i--) {
-		j = Math.floor(Math.random() * (i + 1));
-		x = a[i];
-		a[i] = a[j];
-		a[j] = x;
-	}
-	return a;
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
 export function newAmount(curr, value) {
-	const calc = curr + value;
-	return limitNumberWithinRange(calc, 0, 100);
+  const calc = curr + value;
+  return limitNumberWithinRange(calc, 0, 100);
 }
 
 export function effectAmount(curr, value) {
-	let randArr = shuffle(arr);
-	let number = Math.floor(Math.random() * randArr.length);
-	let index = randArr[number];
-	let calc = curr;
-	if (index !== 0) {
-		calc = curr - value / randArr[number];
-	}
-	return limitNumberWithinRange(calc, 0, 100);
+  let randArr = shuffle(arr);
+  let number = Math.floor(Math.random() * randArr.length);
+  let index = randArr[number];
+  let calc = curr;
+  if (index !== 0) {
+    calc = curr - value / randArr[number];
+  }
+  return limitNumberWithinRange(calc, 0, 100);
 }
 
 export function changeAttribute(attribute, itemValue, pet) {
-	let data = {};
-	switch (attribute) {
-		case 'fun':
-			return (data = {
-				fun: newAmount(pet.fun, itemValue),
-				hunger: effectAmount(pet.hunger, itemValue),
-				energy: effectAmount(pet.energy, itemValue),
-				hygiene: effectAmount(pet.hygiene, itemValue),
-			});
-		case 'hunger':
-			return (data = {
-				hunger: newAmount(pet.hunger, itemValue),
-				fun: effectAmount(pet.fun, itemValue),
-				energy: effectAmount(pet.energy, itemValue),
-			});
-		case 'health':
-			return (data = {
-				health: newAmount(pet.health, itemValue),
-				hunger: effectAmount(pet.hunger, itemValue),
-			});
-		default:
-			return (data = { ...pet });
-	}
+  /* eslint-disable */
+  let data = {};
+  switch (attribute) {
+    case "fun":
+      return (data = {
+        fun: newAmount(pet.fun, itemValue),
+        hunger: effectAmount(pet.hunger, itemValue),
+        energy: effectAmount(pet.energy, itemValue),
+        hygiene: effectAmount(pet.hygiene, itemValue),
+      });
+    case "hunger":
+      return (data = {
+        hunger: newAmount(pet.hunger, itemValue),
+        fun: effectAmount(pet.fun, itemValue),
+        energy: effectAmount(pet.energy, itemValue),
+      });
+    case "health":
+      return (data = {
+        health: newAmount(pet.health, itemValue),
+        hunger: effectAmount(pet.hunger, itemValue),
+      });
+    default:
+      return (data = { ...pet });
+  }
 }
