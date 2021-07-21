46ca9e7de86ce5f797a9ed6b42274fa5b4592967
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-07-21 00:44:30 -0400

    new heroku backend url

--- src/state/actions/Constants.js
+++ src/state/actions/Constants.js
@@ -1,15 +1,15 @@
 const prod = {
-	url: {
-		BASE_URL: 'https://vast-plains-99264.herokuapp.com/api/v1/',
-		AUTH_URL: 'https://vast-plains-99264.herokuapp.com/',
-	},
+  url: {
+    BASE_URL: "https://young-harbor-63152.herokuapp.com/api/v1/",
+    AUTH_URL: "https://young-harbor-63152.herokuapp.com/",
+  },
 };
 
 const dev = {
-	url: {
-		BASE_URL: 'http://localhost:3090/api/v1/',
-		AUTH_URL: 'http://localhost:3090/',
-	},
+  url: {
+    BASE_URL: "http://localhost:3090/api/v1/",
+    AUTH_URL: "http://localhost:3090/",
+  },
 };
 
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export const config = process.env.NODE_ENV === "development" ? dev : prod;
